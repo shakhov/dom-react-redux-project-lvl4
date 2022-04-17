200e17fa3c8380a68915595200312dd8bef7d3d9
shakhov <shahov.a.a@gmail.com>
2022-04-17 21:58:02 +0300

    Add navbar

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -3,17 +3,55 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  Navigate,
+  Link,
   useLocation,
 } from 'react-router-dom';
+
+import {
+  Button,
+  Navbar,
+  Nav,
+  Container,
+} from 'react-bootstrap';
 
 import ChatPage from '../pages/ChatPage.jsx';
 import LoginPage from '../pages/LoginPage.jsx';
 import NotFoundPage from '../pages/NotFoundPage.jsx';
 
+function AuthButton() {
+  const auth = {loggedIn: false};
+  const username = 'Username';
+  const location = useLocation();
+
+  if (auth.loggedIn) {
+    return (
+      <Nav>
+        <Navbar.Text>
+          Logged in as &nbsp;
+          <a href="#login">{username}</a>
+        </Navbar.Text>
+        &nbsp;
+        <Button>
+          Log Out
+        </Button>
+      </Nav>
+    );
+  }
+  return <Button as={Link} to="/login" state={{ from: location }}>Log In</Button>;
+}
+
 function Router() {
   return (
     <BrowserRouter>
-      <h1>Navbar placeholder</h1>
+      <Navbar className="shadow-sm navba-expand-lg navbar-light bg-white p-3">
+        <Container>
+          <Navbar.Brand as={Link} to="/" className="fs-2">
+            Chat
+          </Navbar.Brand>
+          <AuthButton />
+        </Container>
+      </Navbar>
       <Routes>
         <Route
           path="/"
