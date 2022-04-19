0b3bda898ec9f26dc6fa087e05a2d3d495bb1c46
shakhov <shahov.a.a@gmail.com>
2022-04-20 00:43:16 +0300

    Show channels names on chat page

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import useAuth from '../hooks/useAuth.jsx';
+import fetchData from '../slices/fetchData.js';
+import { selectors as channelsSelectors } from '../slices/channelsSlice.js';
 
 function ChatPage() {
-  return <h1>Chat here!</h1>;
+  const auth = useAuth();
+  const dispatch = useDispatch();
+  const channels = useSelector(channelsSelectors.selectAll);
+
+  useEffect(() => {
+    const { token } = auth.userId;
+    const header = { Authorization: `Bearer ${token}` };
+    dispatch(fetchData({ header }));
+  }, []);
+
+  return (
+      <div>
+      <h4>Channels:</h4>
+      <ul>
+        {channels.map(({ id, name }) => <li key={id}>#{name}</li>)}
+      </ul>
+    </div>
+  );
 }
 
 export default ChatPage;
