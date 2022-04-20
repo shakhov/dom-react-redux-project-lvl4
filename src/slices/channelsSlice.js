cd12ef51b4f054064099d8a65c8f5408180955db
shakhov <shahov.a.a@gmail.com>
2022-04-20 10:23:00 +0300

    Slices include loading status and errors

--- src/slices/channelsSlice.js
+++ src/slices/channelsSlice.js
@@ -2,7 +2,11 @@ import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
 import fetchData from './fetchData.js';
 
 const channelsAdapter = createEntityAdapter();
-const initialState = channelsAdapter.getInitialState();
+const initialState = channelsAdapter.getInitialState({
+  currentChannelId: 1,
+  loadingStatus: 'idle',
+  loadingError: null,
+});
 
 const channelsSlice = createSlice({
   name: 'channels',
@@ -11,12 +15,18 @@ const channelsSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase(fetchData.pending, (state, { payload }) => {
+      .addCase(fetchData.pending, (state) => {
+        state.loadingStatus = 'loading';// eslint-disable-line
+        state.loadingError = null; // eslint-disable-line
       })
       .addCase(fetchData.fulfilled, (state, { payload }) => {
+        state.loadingStatus = 'success'; // eslint-disable-line
+        state.currentChannelId = payload.currentChannelId;// eslint-disable-line
         channelsAdapter.addMany(state, payload.channels);
       })
-      .addCase(fetchData.rejected, (state, { payload }) => {
+      .addCase(fetchData.rejected, (state, { error }) => {
+        state.loadingStatus = 'error'; // eslint-disable-line
+        state.loadingError = error.message;// eslint-disable-line
       });
   },
 });
--- src/slices/messagesSlice.js
+++ src/slices/messagesSlice.js
@@ -2,7 +2,10 @@ import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
 import fetchData from './fetchData.js';
 
 const messagesAdapter = createEntityAdapter();
-const initialState = messagesAdapter.getInitialState();
+const initialState = messagesAdapter.getInitialState({
+  loadingStatus: 'idle',
+  loadingError: null,
+});
 
 const messagesSlice = createSlice({
   name: 'messages',
@@ -11,12 +14,17 @@ const messagesSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase(fetchData.pending, (state, { payload }) => {
+      .addCase(fetchData.pending, (state) => {
+        state.loadingStatus = 'loading';// eslint-disable-line
+        state.loadingError = null; // eslint-disable-line
       })
       .addCase(fetchData.fulfilled, (state, { payload }) => {
+        state.loadingStatus = 'success'; // eslint-disable-line
         messagesAdapter.addMany(state, payload.messages);
       })
-      .addCase(fetchData.rejected, (state, { payload }) => {
+      .addCase(fetchData.rejected, (state, { error }) => {
+        state.loadingStatus = 'error'; // eslint-disable-line
+        state.loadingError = error.message;// eslint-disable-line
       });
   },
 });
