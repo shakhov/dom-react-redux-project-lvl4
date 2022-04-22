e1a0886e2eee8d2eb44ca363ebf2f28abc127af9
shakhov <shahov.a.a@gmail.com>
2022-04-22 18:13:28 +0300

    Refactor selectors

--- src/slices/channelsSlice.js
+++ src/slices/channelsSlice.js
@@ -35,13 +35,7 @@ const channelsSlice = createSlice({
       .addCase(fetchData.fulfilled, (state, { payload }) => {
         state.loadingStatus = 'success'; // eslint-disable-line
         state.currentChannelId = payload.currentChannelId;// eslint-disable-line
-        channelsAdapter.addMany(
-          state,
-          [...payload.channels,
-           { id: 3, name: 'test', removable: true },
-           { id: 4, name: 'foobar', removable: true },
-          ],
-        );
+        channelsAdapter.addMany(state, payload.channels);
       })
       .addCase(fetchData.rejected, (state, { error }) => {
         state.loadingStatus = 'error'; // eslint-disable-line
--- src/slices/messagesSlice.js
+++ src/slices/messagesSlice.js
@@ -1,6 +1,7 @@
 import {
   createSlice,
   createEntityAdapter,
+  createSelector,
 } from '@reduxjs/toolkit';
 
 import fetchData from './fetchData.js';
@@ -27,17 +28,7 @@ const messagesSlice = createSlice({
       })
       .addCase(fetchData.fulfilled, (state, { payload }) => {
         state.loadingStatus = 'success'; // eslint-disable-line
-        messagesAdapter.addMany(
-          state,
-          [...payload.messages,
-           {id: 1, username: 'admin', channelId: 1, body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at velit leo. Cras a nunc nunc. Cras at augue sit amet odio tincidunt mattis. Mauris non pretium felis. Phasellus lobortis lorem vitae eros iaculis, posuere cursus magna lacinia. Donec sapien risus, consequat lacinia scelerisque nec, placerat fringilla urna. Duis in suscipit dui. Sed justo felis, ornare sed turpis at, suscipit mattis magna. Mauris congue rutrum imperdiet. Phasellus risus metus, sodales sed pulvinar sed, auctor ut felis. Suspendisse eu est nec lacus imperdiet fringilla. Duis aliquam scelerisque pulvinar. Nunc posuere, est eu convallis aliquet, ex diam placerat lectus, quis hendrerit lorem est at velit. Nunc placerat ut ligula in facilisis. Vestibulum vel quam ultrices, congue lacus sed, faucibus nunc. Quisque ullamcorper nisl tortor, eget tempor nisl lobortis nec. ' },
-           {id: 2, username: 'guest', channelId: 1, body: 'test 2' },
-           {id: 3, username: 'user1', channelId: 1, body: 'test 3' },
-           {id: 4, username: 'admin', channelId: 2, body: 'test test 1' },
-           {id: 5, username: 'guest', channelId: 2, body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at velit leo. Cras a nunc nunc. Cras at augue sit amet odio tincidunt mattis. Mauris non pretium felis. Phasellus lobortis lorem vitae eros iaculis, posuere cursus magna lacinia. Donec sapien risus, consequat lacinia scelerisque nec, placerat fringilla urna. Duis in suscipit dui. Sed justo felis, ornare sed turpis at, suscipit mattis magna. Mauris congue rutrum imperdiet. Phasellus risus metus, sodales sed pulvinar sed, auctor ut felis. Suspendisse eu est nec lacus imperdiet fringilla. Duis aliquam scelerisque pulvinar. Nunc posuere, est eu convallis aliquet, ex diam placerat lectus, quis hendrerit lorem est at velit. Nunc placerat ut ligula in facilisis. Vestibulum vel quam ultrices, congue lacus sed, faucibus nunc. Quisque ullamcorper nisl tortor, eget tempor nisl lobortis nec. ' },
-           {id: 7, username: 'user2', channelId: 4, body: 'test test 2' },
-          ],
-        );
+        messagesAdapter.addMany(state, payload.messages);
       })
       .addCase(fetchData.rejected, (state, { error }) => {
         state.loadingStatus = 'error'; // eslint-disable-line
@@ -52,8 +43,13 @@ export const selectors = messagesAdapter.getSelectors((state) => state.messages)
 export const selectLoadingStatus = (state) => state.messages.loadingStatus;
 export const selectLoadingError = (state) => state.messages.loadingError;
 
-export const selectMessagesByChannelId = (id) => (state) => (
-  selectors.selectAll(state).filter(({ channelId }) => channelId === id)
+export const selectMessagesByChannelId = (id) => (
+  createSelector(
+    [
+      selectors.selectAll,
+    ],
+    (messages) => messages.filter(({ channelId }) => channelId === id),
+  )
 );
 
 export default messagesSlice.reducer;
